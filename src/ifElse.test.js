6acc23fc3620e8ed7c9e03f00cc625c8db8fe347
Dmytro Koriahin <dkoryahin@gmail.com>
2025-08-24 20:59:09 +0300

    add task solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,38 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
+  let condition, first, second;
 
-  it('should ', () => {
+  beforeEach(() => {
+    condition = jest.fn();
+    first = jest.fn();
+    second = jest.fn();
+  })
 
+  it('should declareted a function', () => {
+    expect(ifElse).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should call 'first' if 'condition' return true`, () => {
+    condition.mockReturnValue(true);
+
+    const result = ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).not.toHaveBeenCalled();
+    expect(result).toBeUndefined();
+  })
+
+  it(`should call 'second' if 'condition' return false`, () => {
+    condition.mockReturnValue(false);
+
+    const result = ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalledTimes(1);
+    expect(result).toBeUndefined();
+  })
 });
